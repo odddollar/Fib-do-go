2c87d646f81cb81fa0b3dab81e19a6be9c17e92e
Simon Eason <sie.eason@gmail.com>
2024-12-16 13:45:50 +0800

    Added basic cli

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module Fibonacci-benchmark
 
 go 1.22.3
+
+require github.com/akamensky/argparse v1.4.0 // indirect
--- main.go
+++ main.go
@@ -2,53 +2,31 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
+	"os"
+
+	"github.com/akamensky/argparse"
 )
 
 // Default values used for benchmark
 const (
-	numWorkers = 30
-	numTasks   = 200
-	fibMin     = 40
-	fibMax     = 45
+	defaultNumWorkers = 30
+	defaultNumTasks   = 200
+	defaultFibMin     = 40
+	defaultFibMax     = 45
 )
 
 func main() {
-	// Save start time
-	start := time.Now()
+	// Create argument parser
+	parser := argparse.NewParser("fib-dot-go", "CPU benchmark that calculates fibonacci numbers")
 
-	// Create buffered channel to hold tasks. Acts like a queue
-	tasks := make(chan int, numTasks)
-
-	// Create wait group to keep running until all tasks complete
-	var wg sync.WaitGroup
-
-	// Push tasks onto queue
-	for i := 0; i < numTasks; i++ {
-		tasks <- fibMin + (i % (fibMax - fibMin))
+	// Parse cli input
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
 	}
 
-	// Spawn workers
-	for i := 1; i <= numWorkers; i++ {
-		// Increment wait group
-		wg.Add(1)
+	// Run benchmark
+	run(defaultNumWorkers, defaultNumTasks, defaultFibMin, defaultFibMax)
 
-		// Give worker id, task channel, and wait group
-		go worker(i, tasks, &wg)
-	}
-
-	// Close channel when no more tasks
-	close(tasks)
-
-	// Wait for all workers to finish
-	wg.Wait()
-
-	// Print elapsed time
-	elapsed := time.Since(start)
-	fmt.Printf("Time taken: %s\n", elapsed)
-
-	// Wait for keypress to exit
-	fmt.Print("Press ENTER to exit")
-	fmt.Scanln()
 }
